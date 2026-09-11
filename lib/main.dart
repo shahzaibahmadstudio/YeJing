9d3b421636dc8aaa013f4c8c9d6537bfba3f024b
Shahzaib Ahmad <shahzaibahmad.studio@gmail.com>
2026-09-11 08:46:12 +0500

    feat: Implementing the native splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const YeJingApp());
 }
 
-class YeJingApp extends StatelessWidget {
+class YeJingApp extends StatefulWidget {
   const YeJingApp({super.key});
+
+  @override
+  State<YeJingApp> createState() => _YeJingAppState();
+}
+
+class _YeJingAppState extends State<YeJingApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    splashInitialization();
+  }
+
+  void splashInitialization() async {
+    await Future.delayed(const Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
